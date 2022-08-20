6c93dbf4c8104f0867cd4e820bba383d5f5ed9bf
RosalieWessels <rosalie.wessels@yahoo.com>
2022-08-20 11:32:37 -0700

    Initial Commit

---
+++ Shared/Password_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password_GeneratorApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Password_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
